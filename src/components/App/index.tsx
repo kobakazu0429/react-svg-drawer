a6a80250d51de1738e2f0eb025c3178f0fb8b9af
kobakakzu0429 <kobakazu10@gmail.com>
2020-01-18 23:27:38 +0900

    add router and top page

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,17 @@
 import React, { FC } from "react";
 import { ThemeProvider } from "@/theme/ThemeProvider";
+import { createRouter } from "@/routes";
+import TopPage from "@/pages/TopPage";
+
+const routes = [
+  {
+    exact: true,
+    path: "/",
+    component: TopPage
+  }
+];
+
+const Router = createRouter({ routes });
 
 const App: FC = () => {
   return <ThemeProvider themeName="default">{Router}</ThemeProvider>;
